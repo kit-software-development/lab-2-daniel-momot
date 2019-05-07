06c083ba6455eb4f3ed196e9cc73a84948b9e7fb
daniel-momot <daniel.momot@gmail.com>
2019-05-07 18:52:11 +0300

    Выполненная лабораторная работа
    В прошлый раз коммит не удался, что я обнаружил только сейчас. Исправляюсь

--- Implementation/HR/Builders.cs
+++ Implementation/HR/Builders.cs
@@ -1,4 +1,5 @@
-using System;
+using Practice.Common;
+using Practice.Organization;
 
 namespace Practice.HR
 {
@@ -8,6 +9,97 @@ namespace Practice.HR
     /// </summary>
     public static class Builders
     {
+
+
+        private class C_ClientBuilder : IClientBuilder
+        {
+            private IClient client;
+            private bool discount_set = false;
+            public C_ClientBuilder()
+            {
+                client = new Client();
+               
+            }
+
+            IClient IBuilder<IClient>.Build()
+            {
+                if (isValid())
+                    return client;
+                else return null;
+            }
+
+            private bool isValid() => !client.Name.IsInvalid && discount_set;
+
+            IClientBuilder IClientBuilder.Discount(float discount)
+            {
+                client.Discount = discount;
+                discount_set = true;
+                return this;
+            }
+
+            IClientBuilder IClientBuilder.Name(IName name)
+            {
+                client.Name = name;
+                return this;
+            }
+
+            IClientBuilder IClientBuilder.Name(string name, string surname, string patronymic)
+            {
+                Name n = new Name(name, surname, patronymic);
+                client.Name = n;
+                return this;
+            }
+        }
+
+        private class C_EmployeeBuilder : IEmployeeBuilder
+        {
+            private IEmployee employee;
+            private bool department_set = false;
+            public C_EmployeeBuilder()
+            {
+                employee = new Employee();
+            }
+
+            private bool isValid() => !employee.Name.IsInvalid && department_set;
+
+            IEmployee IBuilder<IEmployee>.Build()
+            {
+                if (isValid())
+                    return employee;
+                else return null;
+            }
+
+            IEmployeeBuilder IEmployeeBuilder.Department(IDepartment department)
+            {
+                employee.Department = department;
+                department_set = true;
+                return this;
+            }
+
+            IEmployeeBuilder IEmployeeBuilder.Department(string department)
+            {
+                employee.Department = new Department(department);
+                department_set = true;
+                return this;
+            }
+
+            IEmployeeBuilder IEmployeeBuilder.Name(IName name)
+            {
+                employee.Name = name;
+                return this;
+            }
+
+            IEmployeeBuilder IEmployeeBuilder.Name(string name, string surname, string patronymic)
+            {
+                employee.Name = new Name(name, surname, patronymic);
+                return this;
+            }
+        }
+
+
+
+
+
         /// <summary>
         ///     Возвращает экземпляр "Строителя" клиентов.
         /// </summary>
@@ -18,8 +110,13 @@ namespace Practice.HR
         {
             /*
              * TODO #6: Реализовать фабричный метод ClientBuilder класса Builders
+             * DONE
              */
-            throw new NotImplementedException();
+
+
+            IClientBuilder builder = new C_ClientBuilder();
+            return builder;
+
         }
 
         /// <summary>
@@ -32,8 +129,10 @@ namespace Practice.HR
         {
             /*
              * TODO #7: Реализовать фабричный метод EmployeeBuilder класса Builders
+             * DONE
              */
-            throw new NotImplementedException();
+            IEmployeeBuilder builder = new C_EmployeeBuilder();
+            return builder;
         }
 
     }
--- Implementation/Organization/Department.cs
+++ Implementation/Organization/Department.cs
@@ -1,12 +1,28 @@
-﻿namespace Practice.Organization
+﻿using System;
+
+namespace Practice.Organization
 {
     /// <summary>
     ///     Скрытая реализация представления об отделе предприятия.
     /// </summary>
-    internal struct Department
+    internal struct Department : IDepartment
     {
         /*
          * TODO #2: Реализуйте интерфейс IDepartment для структуры Department
+         * DONE
          */
+         private string _department;
+        string IDepartment.Name
+        {
+            get
+            {
+                return _department;
+            }
+        }
+
+        public Department(string department)
+        {
+            _department = department;
+        }
     }
 }
